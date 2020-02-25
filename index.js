8941f238baea9ba5952302a5c69633823e3bf97a
alexst@divvyhomes.com <alexstrachan@Alexs-MacBook-Pro-2.local>
2020-02-24 16:20:13 -0800

    lint to our own standards

--- index.js
+++ index.js
@@ -6,24 +6,24 @@
 // ESLint configuration
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   extends: [
-    "airbnb",
-    "plugin:flowtype/recommended",
-    "plugin:css-modules/recommended",
-    "prettier",
-    "prettier/flowtype",
-    "prettier/react",
-    "plugin:jest/recommended"
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'plugin:css-modules/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+    'plugin:jest/recommended',
   ],
 
   plugins: [
-    "flowtype",
-    "css-modules",
-    "prettier",
-    "react-hooks",
-    "no-only-tests",
-    "jest"
+    'flowtype',
+    'css-modules',
+    'prettier',
+    'react-hooks',
+    'no-only-tests',
+    'jest',
     /* NOTE: adding rules as ESLint plugin like this didn't work in the context of NPM
      * package. After we published a new version with added "eslint-plugin-divvy-rules"
      * folder it broke builds as consumers couldn't install the library anymore. Error that
@@ -36,109 +36,106 @@ module.exports = {
 
   globals: {
     __DEV__: true,
-    __GIT_COMMITHASH__: true
+    __GIT_COMMITHASH__: true,
   },
 
   env: {
     browser: true,
-    "jest/globals": true
+    'jest/globals': true,
   },
 
   rules: {
     // `js` and `jsx` are common extensions
     // `mjs` is for `universal-router` only, for now
-    "import/extensions": [
-      "error",
-      "always",
+    'import/extensions': [
+      'error',
+      'always',
       {
-        ts: "never",
-        tsx: "never",
-        js: "never",
-        jsx: "never",
-        mjs: "never"
-      }
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+      },
     ],
 
     // Enforce strict equality, even against null literal
     // https://eslint.org/docs/rules/eqeqeq
-    eqeqeq: ["error", "always"],
+    eqeqeq: ['error', 'always'],
 
-    "max-len": "off",
+    'max-len': 'off',
 
     // Not supporting nested package.json yet
     // https://github.com/benmosher/eslint-plugin-import/issues/458
-    "import/no-extraneous-dependencies": "off",
+    'import/no-extraneous-dependencies': 'off',
 
     // Recommend not to leave any console.log in your code
     // Use console.error, console.warn and console.info instead
-    "no-console": [
-      "error",
+    'no-console': [
+      'error',
       {
-        allow: ["warn", "error", "info"]
-      }
+        allow: ['warn', 'error', 'info'],
+      },
     ],
 
-    "no-restricted-syntax": "off",
-    "no-await-in-loop": "off",
-    "no-debugger": "off",
-    "prefer-destructuring": "off",
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+    'no-debugger': 'off',
+    'prefer-destructuring': 'off',
 
     // a11y removed rule, ignore them
-    "jsx-a11y/href-no-hash": "off",
+    'jsx-a11y/href-no-hash': 'off',
 
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/308#issuecomment-322954274
-    "jsx-a11y/label-has-for": "warn",
-    "jsx-a11y/click-events-have-key-events": "never",
-    "jsx-a11y/mouse-events-have-key-events": "never",
-    "jsx-a11y/anchor-is-valid": "never",
-    "jsx-a11y/label-has-associated-control": "never",
-    "jsx-a11y/no-target-blank": "never",
+    'jsx-a11y/label-has-for': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'never',
+    'jsx-a11y/mouse-events-have-key-events': 'never',
+    'jsx-a11y/anchor-is-valid': 'never',
+    'jsx-a11y/label-has-associated-control': 'never',
+    'jsx-a11y/no-target-blank': 'never',
 
     // Allow js files to use jsx syntax, too
-    "react/jsx-filename-extension": [
-      "error",
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
-    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
 
     // Automatically convert pure class to function by
     // babel-plugin-transform-react-pure-class-to-function
     // https://github.com/kriasoft/react-starter-kit/pull/961
-    "react/prefer-stateless-function": "off",
-    "react/destructuring-assignment": "never",
-    "react/default-props-match-prop-types": "never",
-    "react/no-access-state-in-setstate": "never",
-    "react/require-default-props": "never",
-    "react/no-unescaped-entities": "never",
-    "react/sort-comp": "never",
-    "react/button-has-type": "never",
-    "react/no-unused-state": "never",
-    "react/jsx-no-target-blank": "never",
+    'react/prefer-stateless-function': 'off',
+    'react/destructuring-assignment': 'never',
+    'react/default-props-match-prop-types': 'never',
+    'react/no-access-state-in-setstate': 'never',
+    'react/require-default-props': 'never',
+    'react/no-unescaped-entities': 'never',
+    'react/sort-comp': 'never',
+    'react/button-has-type': 'never',
+    'react/no-unused-state': 'never',
+    'react/jsx-no-target-blank': 'never',
 
     // https://www.npmjs.com/package/eslint-plugin-react-hooks
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
 
     // ESLint plugin for prettier formatting
     // https://github.com/prettier/eslint-plugin-prettier
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         // https://github.com/prettier/prettier#options
         singleQuote: true,
-        trailingComma: "all",
-        printWidth: 100
-      }
+        trailingComma: 'all',
+        printWidth: 100,
+      },
     ],
 
-    "no-param-reassign": ["error", { props: false }],
-    "no-plusplus": "off",
-    "prefer-rest-params": "warn",
+    'no-param-reassign': ['error', { props: false }],
+    'no-plusplus': 'off',
+    'prefer-rest-params': 'warn',
 
     // reasoning around this rule is weak https://github.com/airbnb/javascript/issues/1365
     // but it complicates refactoring in certain cases
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
 
-    "no-only-tests/no-only-tests": "error"
+    'no-only-tests/no-only-tests': 'error',
 
     // "divvy-rules/use-core-error": "warn",
     // "divvy-rules/use-user-facing-error": "warn",
@@ -147,18 +144,18 @@ module.exports = {
 
   settings: {
     // https://github.com/johvin/eslint-import-resolver-alias
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        map: [["@app", "./src"]],
-        extensions: [".ts", ".js", ".jsx", ".json"]
+        map: [['@app', './src']],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
       },
       node: {
-        moduleDirectory: ["node_modules", "src"],
-        extensions: [".ts", ".js", ".jsx", ".json"]
-      }
+        moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
+      },
     },
-    "import/parsers": {
-      "typescript-eslint-parser": [".ts", ".tsx"]
-    }
-  }
+    'import/parsers': {
+      'typescript-eslint-parser': ['.ts', '.tsx'],
+    },
+  },
 };
